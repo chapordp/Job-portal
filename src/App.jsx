8ba9109ba4d49ae9332d28994696f519cac89018
Chapo <chapordp400@outlook.com>
2026-02-03 05:33:48 -0600

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/apply" element={<Apply />} />
       </Routes>
+      {/* <Footer /> */}
     </BrowserRouter>
   )
 }
