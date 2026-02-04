98eca7124e1024ab22c3acb89a552456c2fc543d
Chapo <chapordp400@outlook.com>
2026-02-04 08:17:07 -0600

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Jobs from './jobs'
-import Contact from './Contact'
+// import Contact from './contact'
 
 export default function Hero() {
   return (
